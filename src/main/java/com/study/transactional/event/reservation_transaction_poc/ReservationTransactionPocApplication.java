260cea00fefab773c30a6ea21029e480a5398614
dev-chpark <xpmxf5@gmail.com>
2025-06-16 23:07:37 +0900

    🎉 init: basic initial env settings done

---
+++ src/main/java/com/study/transactional/event/reservation_transaction_poc/ReservationTransactionPocApplication.java
@@ -0,0 +1,13 @@
+package com.study.transactional.event.reservation_transaction_poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationTransactionPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationTransactionPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/transactional/event/reservation_transaction_poc/ReservationTransactionPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.transactional.event.reservation_transaction_poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationTransactionPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
